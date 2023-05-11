4c6e209794cd53dec311e83075f165ed7f63e6b5
yd2058 <yd2058@bs.amalnet.k12.il>
2023-05-11 19:00:08 +0300

    update to imagebuttons

--- app/src/main/java/com/example/listenertest/MainActivity.java
+++ app/src/main/java/com/example/listenertest/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.listenertest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btnb, btnr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
